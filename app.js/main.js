225d580aba357ee0207ea997c3c1cfb0aba30da0
LilliTheDev <lillithedev@gmail.com>
2022-04-09 23:29:09 -0400

    Coding challenge for 04-09-2022

---
+++ app.js/main.js
@@ -0,0 +1,13 @@
+//Write a function called repeatStr which repeats the given string string exactly n times.
+
+//Example & Test Cases
+//repeatStr(6, "I") // "IIIIII"
+//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+//Create function that takes in a string and a number as parameters
+//Want to concatenate or repeat the string (n) number of times
+//Ignore spacing & case sensitivity
+
+function repeatStr(n, s){
+  return s.repeat(n);
+}
